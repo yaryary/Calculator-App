202220aaad2651c87b7d87b1e13c0b2e9b5ba8ae
yaryary <ybust005@fiu.edu>
2024-11-04 06:02:02 -0500

    Completed calculator app

--- Calculator.java
+++ Calculator.java
@@ -5,17 +5,191 @@ import java.awt.event.*;
 
 public class Calculator implements ActionListener{
 
+    JFrame frame;
+    JTextField textField;
+    JButton[] numButtons = new JButton[10];
+    JButton[] funButtons = new JButton[9];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton, negButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free", Font.BOLD,30);
+
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
+
+
+
     Calculator(){
 
+        frame = new JFrame("Calculator"); //frame title
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //close out program
+        frame.setSize(420, 550);
+        frame.setLayout(null); // no layout manager
+
+
+        textField = new JTextField();
+        textField.setBounds(50,25,300,50); //set bounds due to no layout manager
+        textField.setFont(myFont);
+        textField.setEditable(false);
+
+        // creating JButtons
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+        negButton = new JButton("( - )");
+
+
+        // assigning buttons to function array
+        funButtons[0] = addButton;
+        funButtons[1] = subButton;
+        funButtons[2] = mulButton;
+        funButtons[3] = divButton;
+        funButtons[4] = decButton;
+        funButtons[5] = equButton;
+        funButtons[6] = delButton;
+        funButtons[7] = clrButton;
+        funButtons[8] = negButton;
+
+        for (int i = 0; i < 9; i++){
+            funButtons[i].addActionListener(this);
+            funButtons[i].setFont(myFont);
+            funButtons[i].setFocusable(false); // prevent outline around buttons
+        }
+
+        // assign number buttons to array
+        for (int i = 0; i < 10; i++){
+            numButtons[i] = new JButton(String.valueOf(i));
+            numButtons[i].addActionListener(this);
+            numButtons[i].setFont(myFont);
+            numButtons[i].setFocusable(false);
+        }
+
+        // set bounds for bottom function buttons
+        negButton.setBounds(50,430,100,50);
+        delButton.setBounds(150,430,100,50);
+        clrButton.setBounds(250,430,100,50);
+
+        // create panel grid for buttons
+        panel = new JPanel();
+        panel.setBounds(50,100,300,300);
+        panel.setLayout(new GridLayout(4,4,10,10));
+
+        // intentional order, depends on you prefer the layout
+        panel.add(numButtons[1]);
+        panel.add(numButtons[2]);
+        panel.add(numButtons[3]);
+        panel.add(addButton);
+
+        panel.add(numButtons[4]);
+        panel.add(numButtons[5]);
+        panel.add(numButtons[6]);
+        panel.add(subButton);
+
+        panel.add(numButtons[7]);
+        panel.add(numButtons[8]);
+        panel.add(numButtons[9]);
+        panel.add(mulButton);
+
+        panel.add(decButton);
+        panel.add(numButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+
+        frame.add(panel);
+        frame.add(negButton);
+        frame.add(delButton);
+        frame.add(clrButton);
+        frame.add(textField);
+        frame.setVisible(true);
+
     }
+
     public static void main(String[] args){
 
-        
+        Calculator c = new Calculator();
 
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) { // assigning actions to the buttons
 
+        for (int i = 0; i < 10; i++){
+            if(e.getSource() == numButtons[i]) {
+                textField.setText(textField.getText().concat(String.valueOf(i)));
+            }
+        }
+
+        if(e.getSource() == decButton) {
+            textField.setText(textField.getText().concat("."));
+        }
+
+        if(e.getSource() == addButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '+';
+            textField.setText("");
+        }
+
+        if(e.getSource() == subButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '-';
+            textField.setText("");
+        }
+
+        if(e.getSource() == mulButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '*';
+            textField.setText("");
+        }
+
+        if(e.getSource() == divButton) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '/';
+            textField.setText("");
+        }
+
+        // assigning actions to equal button depending on operator used
+        if(e.getSource() == equButton) {
+            num2 = Double.parseDouble(textField.getText());
+
+            switch(operator){
+                case'+':
+                    result = num1 + num2;
+                    break;
+                case'-':
+                    result = num1 - num2;
+                    break;
+                case'*':
+                    result = num1 * num2;
+                    break;
+                case'/':
+                    result = num1 / num2;
+                    break;
+            }
+            textField.setText(String.valueOf(result));
+            num1 = result;
+        }
+        if(e.getSource() == clrButton) {
+            textField.setText("");
+        }
+
+        if(e.getSource() == delButton) {
+            String string = textField.getText();
+            textField.setText("");
+            for (int i = 0; i < string.length() - 1; i++) {
+                textField.setText(textField.getText() + string.charAt(i));
+            }
+        }
+        if(e.getSource() == negButton) {
+            double temp = Double.parseDouble(textField.getText());
+            temp *= -1;
+            textField.setText(String.valueOf(temp));
+        }
     }
 }
